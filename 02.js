60934c9bf59ea16a22daf0e740293e6e8712a22f
Diego Toro <diegotoro6789@gmail.com>
2023-07-18 18:41:36 -0500

    update

---
+++ 02.js
@@ -0,0 +1,18 @@
+window.onload = function () {
+    let btn = document.querySelector(".ln") 
+
+    const tablavalor = () => {
+
+
+        if (btn = ((65 || 49) & (66 || 50))
+        ) {
+           alert("ok")
+        } else {
+            alert("equivocado")
+        }
+        
+    }
+    btn.onclick = function () {
+        tablavalor();
+    }
+}
